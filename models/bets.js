69a9c0c6c3a934086f7ce94cc48e070323793386
Spencer Mogil <54917317+smogil12@users.noreply.github.com>
2020-02-23 14:32:40 -0500

    chore: fix code in routes, models, and index (#18)

--- models/bets.js
+++ models/bets.js
@@ -1,11 +1,13 @@
-const sequelize = require("../config");
-const { DataTypes } = require("sequelize");
+// const sequelize = require("../config");
+// const { DataTypes } = require("sequelize");
 
-const Bet = sequelize.define("Bet", {
-  userPick: DataTypes.String,
-  allowNull: false
-});
-
-Bet.sync();
+module.exports = function(sequelize, DataTypes) {
+  const Bet = sequelize.define("Bet", {
+    userPick: DataTypes.String,
+    allowNull: false
+  });
+  return Bet;
+};
+// Bet.sync();
 
 module.exports = Bet;
