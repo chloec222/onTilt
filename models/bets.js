7baf91a6019d41f005c152493e607b1cd2aade6d
Spencer Mogil <54917317+smogil12@users.noreply.github.com>
2020-02-24 23:33:40 -0500

    feat: bets api route working. can get and post (#27)

--- models/bets.js
+++ models/bets.js
@@ -3,24 +3,8 @@
 
 module.exports = function(sequelize, DataTypes) {
   const Bet = sequelize.define("Bet", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    body: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    category: {
-      type: DataTypes.STRING,
-      defaultValue: "Personal"
-    }
+    teamName: DataTypes.STRING,
+    wager: DataTypes.INTEGER
   });
   return Bet;
 };
