ca4fa84f12f99483714bc346da60cc8c749e2edf
Spencer Mogil <smogil12@gmail.com>
2020-02-25 20:02:51 -0500

    feat: now can delete users and bets by id, and search by id

--- routes/bets.js
+++ routes/bets.js
@@ -13,4 +13,24 @@ module.exports = function(app) {
       res.json(dbBet);
     });
   });
+  app.get("/api/bets/:id", function(req, res) {
+    // Find one Author with the id in req.params.id and return them to the user with res.json
+    db.Bet.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbBet) {
+      res.json(dbBet);
+    });
+  });
+  app.delete("/api/bets/:id", function(req, res) {
+    // Delete the Author with the id available to us in req.params.id
+    db.Bet.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbBets) {
+      res.json(dbBets);
+    });
+  });
 };
